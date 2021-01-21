aef1ecf0d53d3a0e3a5b9008bc59b173b2c2b892
ANA-PC\ana <ana@chromaviso.com>
2021-01-21 14:54:46 +0100

    -ANA

--- AudioManager.lua
+++ AudioManager.lua
@@ -2,7 +2,7 @@
 AudioManager = {neighbours = {},
                 masking = {},
 
-                isActive = true,
+                isActive = false,
 
                 new = function ()
                     log("ERROR AudioManager is a singleton object with global access")
@@ -13,7 +13,7 @@ function AudioManager:init()
 
     self.isActive = false
     self.masking = {false, false, false, false, false}
-    -- group            1      2       3       4     5
+    -- group            1      2       3       4     5  
     self.neighbours = {{2}, {1, 3}, {2, 4}, {3, 5}, {4}}
 
 end
